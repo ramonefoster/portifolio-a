ecb281dd850b29fa3a8d6f80b4ee8edd2becec8e
Ramones <you@example.com>
2021-07-09 12:58:37 -0300

    add index commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,12 +10,8 @@ router.get('/index', (req, res) => {
     res.render('index')
 })
 
-router.get('/profile', (req, res) => {
-    res.render('profile')
-})
 
-router.get('/teste', (req, res) => {
-    res.render('teste')
-})
+
+
 
 module.exports = router
